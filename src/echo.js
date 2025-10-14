73c43f8f4d3a544bfdc94af3bca9dbd3e5e8de77
Sầm Văn Mạnh <manhsvps38793@gmail.com>
2025-10-14 13:32:11 +0700

    Lưu thay đổi trước khi lấy code mới

---
+++ src/echo.js
@@ -0,0 +1,13 @@
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: process.env.REACT_APP_PUSHER_APP_KEY,
+    cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
+
+export default window.Echo; 
